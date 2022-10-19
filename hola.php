ec6a57df3f2071faf312d5efb8fad21d3b474157
Juan <juanjtp92@gmail.com>
2022-10-19 15:42:31 +0200

    inicio de proyecto de pruebas

---
+++ hola.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Nuestro primer html</title>
+  </head>
+  <body>
+    <?php
+        echo "Hi I am a PHP script";
+     ?>
+  </body>
+</html>
